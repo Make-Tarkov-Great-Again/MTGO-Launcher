206500b88443e79e999dcaa0cc6b714ab9d2ec7d
Kes <70953258+EFHDev@users.noreply.github.com>
2023-08-20 03:08:20 -0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/workshop/mod/:id',
+            component: () => import('@/components/ModPageComponent.vue'),
+            name: 'modpage',
+            props: true
+        }
+    ]
+
+})
+
 
 createApp(App)
   .use(router)
