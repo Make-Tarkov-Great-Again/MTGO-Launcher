736f2950bab7e08d44c6f31353bdda2ac508a1c4
Kes <70953258+EFHDev@users.noreply.github.com>
2023-08-25 16:07:32 -0400

    Update launching.go

--- backend/Tarkov/launching.go
+++ backend/Tarkov/launching.go
@@ -3,3 +3,6 @@ package launch
 //TODO: AKI/MTGA Launching - Automatic server start on launch
 //TODO: AKI/MTGA modded profile check (Can be done entirely client side via saving profiles)
 //TODO: Missing mod warnings
+//TODO: Listen for tarkov to launch and close.
+//TODO: Move profile-data before launch.
+//TODO: Copy profile data after game close.
