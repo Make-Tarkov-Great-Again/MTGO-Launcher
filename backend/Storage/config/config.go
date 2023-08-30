3458b7a72b55646519ccda8d348f1d64534e747b
Kes <70953258+EFHDev@users.noreply.github.com>
2023-08-30 15:06:22 -0400

    Storage/Config reference

---
+++ backend/Storage/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+//import (
+//"fmt"
+//storage "mtgolauncher/backend/Storage"
+//)
+
+//func getConfig() {
+//	appdata, err := storage.GetAppDataDir()
+//	if err != nil {
+//		fmt.Println("Error:", err)
+//		return
+//	}
+//}
