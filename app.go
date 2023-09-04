a2005e682765e995fd8994bbc134bdbe7b768832
Kes <70953258+EFHDev@users.noreply.github.com>
2023-09-04 17:12:08 -0400

    [Update] App

--- app.go
+++ app.go
@@ -16,6 +16,7 @@ func NewApp() *App {
 	return &App{}
 }
 
+// @param ctx context.Context
 // Functions to run on startup
 func (a *App) startup(ctx context.Context) {
 	app := launcher.NewLauncher()
@@ -36,6 +37,21 @@ func (a *App) startup(ctx context.Context) {
 		fmt.Println("No internet connection... Starting in offline mode. Check your firewall if this is a error.")
 		//TODO: Offline mode
 	}
+
+	// Example mod information
+	modID := 1
+	modName := "AKI.AnotherAnimeModelMod"
+	modAuthor := "YourMom"
+	modDescription := "Imanage most of your mods being anime models."
+
+	// Call CreateConfigIfNotExist to create the mod configuration
+	err = app.Config.CreateConfigIfNotExist(modID, modName, modAuthor, modDescription)
+	if err != nil {
+		fmt.Println("Error creating mod configuration:", err)
+		return
+	}
+
+	fmt.Println("Mod configuration created successfully!")
 
 	// TODO: Database check
 	// TODO: If either fails -> Offline mode
