33ac9675e2c87043d40b08b6cf833ac25a19a79e
Kes <70953258+EFHDev@users.noreply.github.com>
2023-09-04 17:10:43 -0400

    [Add[ FindConfigFiles

--- backend/Storage/config/config.go
+++ backend/Storage/config/config.go
@@ -1,14 +1,44 @@
 package config
 
-//import (
-//"fmt"
-//storage "mtgolauncher/backend/Storage"
-//)
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
-//func getConfig() {
-//	appdata, err := storage.GetAppDataDir()
-//	if err != nil {
-//		fmt.Println("Error:", err)
-//		return
-//	}
-//}
+type PackageJSON struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Licence     string `json:"license"`
+	AkiVersion  string `json:"akiVersion"`
+	MtgaVersion string `json:"mtgaVersion"`
+}
+
+func FindConfigFiles(dirPath string) (map[string]string, error) {
+	configFiles := make(map[string]string)
+
+	files, err := os.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			subDir := filepath.Join(dirPath, file.Name())
+			subConfigFiles, err := FindConfigFiles(subDir)
+			if err != nil {
+				return nil, err
+			}
+
+			for key, value := range subConfigFiles {
+				configFiles[key] = value
+			}
+		} else if strings.Contains(strings.ToLower(file.Name()), "config") {
+			fmt.Printf("Found config %s \n", file.Name())
+			configFiles[file.Name()] = filepath.Join(dirPath, file.Name())
+		}
+	}
+
+	return configFiles, nil
+}
