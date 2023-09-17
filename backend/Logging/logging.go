b4c9eb110c8b493870cbf8540498d5f506dfad84
Kes <70953258+EFHDev@users.noreply.github.com>
2023-09-17 15:28:36 -0400

    [Update] Logger - > Flog v3

--- backend/Logging/logging.go
+++ backend/Logging/logging.go
@@ -1,6 +1,6 @@
 /*
 Package logging provides a flexible and extensible logging framework for the MTGO-Launcher application. It supports various log types and allows you to control log output both to files and the console.
-
+FLog v3
 Usage:
 
 	log.Init()
@@ -41,110 +41,113 @@ package logging
 import (
 	"fmt"
 	"log"
+	storage "mtgolauncher/backend/Storage"
 	"os"
 	"path"
 	"runtime"
 	"strings"
 	"time"
-
-	storage "mtgolauncher/backend/Storage"
 )
 
-var logsFolder string
-var logsFolderAppDir string
+type Logger struct {
+	logFolder     string
+	logFileMap    map[string]*os.File
+	logConsole    *log.Logger
+	logFilePrefix string
+}
 
-func init() {
-	var err error
-	logsFolder, err = storage.GetAppDataDir()
-	logsFolderAppDir = path.Join(logsFolder, "/logs")
-	logsFolder = logsFolderAppDir
+var (
+	logger *Logger
+)
+var (
+	successColor  = "\033[38;2;0;175;0m"    // Green
+	warningColor  = "\033[38;2;255;204;0m"  // Yellow
+	logErrorColor = "\033[38;2;250;0;0m"    // Red
+	infoColor     = "\033[38;2;84;175;190m" // Custom color
+	resetColor    = "\033[0m"               // Reset color to default
+)
+
+// Fuck you think it does? launch you to mars? It fucking initilizes the logger
+func Init() error {
+	logFolder, err := storage.GetAppDataDir()
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		return err
 	}
+
+	logger = &Logger{
+		logFolder:  path.Join(logFolder, "logs"),
+		logConsole: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+	}
+
+	if err := logger.initFolders(); err != nil {
+		return err
+	}
+
+	if err := logger.initLogFiles(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-const loggingRoutesFile = ""
-
-var logFileMap = make(map[string]string)
-var flog = make(map[string]func(message string, args ...interface{}))
-
-func GetFlog() map[string]func(message string, args ...interface{}) {
-	return flog
+func (l *Logger) initFolders() error {
+	if _, err := os.Stat(l.logFolder); os.IsNotExist(err) {
+		return os.Mkdir(l.logFolder, 0755)
+	}
+	return nil
 }
 
-func logDating() string {
+func (l *Logger) initLogFiles() error {
+	l.logFileMap = make(map[string]*os.File)
+
+	logTypes := []string{"warn", "error", "info", "debug", "server", "aki", "mtga", "online"}
+
+	for _, logType := range logTypes {
+		logTypeFolder := path.Join(l.logFolder, logType)
+		if _, err := os.Stat(logTypeFolder); os.IsNotExist(err) {
+			if err := os.Mkdir(logTypeFolder, 0755); err != nil {
+				return err
+			}
+		}
+
+		logFileName := fmt.Sprintf("log_%s_%d.log", logType, time.Now().UnixNano())
+		logFilePath := path.Join(logTypeFolder, logFileName)
+		file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+		l.logFileMap[logType] = file
+	}
+
+	return nil
+}
+
+func (l *Logger) logDating() string {
 	now := time.Now()
 	milliseconds := now.Nanosecond() / 1e6
 	return fmt.Sprintf("%02d:%02d:%02d:%03d", now.Hour(), now.Minute(), now.Second(), milliseconds)
 }
 
-func updateLoggingRoutesFile() {
-	logTypeFolders := make(map[string]string)
-
-	logTypes := []string{"warn", "error", "info", "debug", "server", "aki", "mtga", "online"}
-	for _, logType := range logTypes {
-		logTypeFolder := path.Join(logsFolder, logType)
-		logTypeFolders[logType] = logTypeFolder
-
-		if _, err := os.Stat(logTypeFolder); os.IsNotExist(err) {
-			os.Mkdir(logTypeFolder, 0755)
-		}
-	}
-
-	for logType, logTypeFolder := range logTypeFolders {
-		if _, exists := logFileMap[logType]; !exists {
-			logFileName := fmt.Sprintf("log_%s_%d.log", logType, time.Now().UnixNano())
-			logFilePath := path.Join(logTypeFolder, logFileName)
-			logFileMap[logType] = logFilePath
-		}
-	}
-
-	var routes []string
-	for logType, logFileName := range logFileMap {
-		routes = append(routes, fmt.Sprintf("%s:%s", logType, logFileName))
-	}
-	routesContent := strings.Join(routes, "\n")
-	err := os.WriteFile(loggingRoutesFile, []byte(routesContent), 0644)
-	if err != nil {
-		log.Printf("Failed to update logging routes file: %v\n", err)
-	}
-}
-
-func readLoggingRoutesFile() {
-	content, err := os.ReadFile(loggingRoutesFile)
-	if err != nil {
-		//log.Printf("Failed to read logging routes file: %v\n", err) I dont wanna hear "OH OH OH WE CANT READ THAT" x30 and im too lazy to change it. fuck you. i dont even think i used logging routes since fucking verson 1.
-		return
-	}
-
-	routes := strings.Split(string(content), "\n")
-	for _, route := range routes {
-		parts := strings.Split(route, ":")
-		if len(parts) == 2 {
-			logFileMap[parts[0]] = parts[1]
-		}
-	}
-}
-
-func createLogFunction(logType string) func(message string, args ...interface{}) {
-	readLoggingRoutesFile()
-	return func(message string, args ...interface{}) {
-		flogg(logType, message, false, args...)
-	}
-}
-
-func flogg(logType, format string, silent bool, args ...interface{}) {
+func (l *Logger) log(logType string, message string, args ...interface{}) {
 	pc, _, _, _ := runtime.Caller(2)
 	callerFunc := runtime.FuncForPC(pc)
 	callerFuncName := "unknown"
 	callerPackageName := "unknown"
+	silentLogging := false
+
+	// Check if bitch is silent
+	if len(args) > 0 {
+		if val, ok := args[len(args)-1].(bool); ok {
+			silentLogging = val
+			args = args[:len(args)-1] // get that fucking "EXTRA-BOOL=TRUE(MISSING)" out of my nice log output fucker
+		}
+	}
+
 	if callerFunc != nil {
 		callerFuncNameFull := callerFunc.Name()
 		parts := strings.Split(callerFuncNameFull, ".")
 
 		if len(parts) > 1 {
-			//If its a method receiver. (If it has "(*" ) extract the method receivers name, and put it infront of func name so i dont get shit like "Launcher.StartServer" WHICH ONE?
 			if strings.Contains(parts[1], "(*") {
 				receiverPart := strings.Trim(parts[1], "(*")
 				receiverPart = strings.Trim(receiverPart, ")")
@@ -152,7 +155,6 @@ func flogg(logType, format string, silent bool, args ...interface{}) {
 				callerPackageName = packageParts[len(packageParts)-1]
 				callerFuncName = receiverPart + "." + parts[2]
 			} else {
-				// Non-method receiver
 				packageParts := strings.Split(parts[0], "/")
 				callerPackageName = packageParts[len(packageParts)-1]
 				callerFuncName = parts[1] + "." + parts[2]
@@ -160,187 +162,78 @@ func flogg(logType, format string, silent bool, args ...interface{}) {
 		}
 	}
 
-	if _, exists := logFileMap[logType]; !exists {
-		logTypeFolder := path.Join(logsFolder, logType)
-		if _, err := os.Stat(logTypeFolder); os.IsNotExist(err) {
-			os.Mkdir(logTypeFolder, 0755)
-		}
-
-		logFileName := fmt.Sprintf("log_%s_%d.log", logType, time.Now().UnixNano())
-		logFilePath := path.Join(logTypeFolder, logFileName)
-		logFileMap[logType] = logFilePath
+	// define the colors, so like black = uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+	var logTypeColor string
+	switch logType {
+	case "error":
+		logTypeColor = logErrorColor
+	case "warn":
+		logTypeColor = warningColor
+	case "info":
+		logTypeColor = infoColor
+	default:
+		logTypeColor = successColor
 	}
 
-	logFile, err := os.OpenFile(logFileMap[logType], os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Printf("Failed to open log file: %v\n", err)
-		return
-	}
-	defer logFile.Close()
-
-	prefix := fmt.Sprintf("[%s] [%s.%s]:", logDating(), callerPackageName, callerFuncName)
-
-	var argsSlice []interface{}
-	for _, arg := range args {
-		argsSlice = append(argsSlice, arg)
+	if file, exists := l.logFileMap[logType]; exists {
+		prefix := fmt.Sprintf("[%s] [%s.%s]:", l.logDating(), callerPackageName, callerFuncName)
+		message := fmt.Sprintf(message, args...)
+		file.WriteString(prefix + " " + message + "\n")
 	}
 
-	message := fmt.Sprintf(format, argsSlice...)
-	logFile.WriteString(prefix + " " + message + "\n")
-
-	if !silent {
-		fmt.Println(prefix, message)
+	if !silentLogging {
+		logTypePrefix := fmt.Sprintf("[%s]", strings.ToUpper(logType))
+		prefix := fmt.Sprintf("[%s] [%s.%s]:", l.logDating(), callerPackageName, callerFuncName)
+		message := fmt.Sprintf(message, args...)
+		fmt.Printf("%s%s %s%s %s\n", logTypeColor, logTypePrefix, resetColor, prefix, message)
 	}
 }
-func LogInit() {
-	var err error
-	logsFolder, err = storage.GetAppDataDir()
-	logsFolderAppDir = path.Join(logsFolder, "/logs")
-	logsFolder = logsFolderAppDir
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
 
-	// Create the logs folder if it doesn't exist
-	if _, err := os.Stat(logsFolder); os.IsNotExist(err) {
-		os.Mkdir(logsFolder, 0755)
-	}
-
-	// Create logType folders if they don't exist
-	logTypes := []string{"warn", "error", "info", "debug", "server"}
-	for _, logType := range logTypes {
-		logTypeFolder := path.Join(logsFolder, logType)
-		if _, err := os.Stat(logTypeFolder); os.IsNotExist(err) {
-			os.Mkdir(logTypeFolder, 0755)
-		}
-	}
-
-	// Update the logging routes file
-	updateLoggingRoutesFile()
-	readLoggingRoutesFile()
-
-	flog = map[string]func(message string, args ...interface{}){
-		"warn":   createLogFunction("warn"),
-		"error":  createLogFunction("error"),
-		"info":   createLogFunction("info"),
-		"debug":  createLogFunction("debug"),
-		"aki":    createLogFunction("aki"),
-		"mtga":   createLogFunction("mtga"),
-		"online": createLogFunction("online"),
-	}
-
-	flogg("info", "Hello world", true)
-}
-
-// Flog: File Logging -> Info
-//
-// Prints to file and console by default, override it by passing true to make it silent
-//
-//	log.Info("Foobar", true)
+// Info logs an info message
 func Info(message string, args ...interface{}) {
-	silent := false
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("info", message, args...)
 	}
-	flogg("info", message, silent, args...)
 }
 
-// Flog: File Logging -> Info
-//
-// Prints to file and console by default, override it by passing true to make it silent
-//
-//	log.Warn("Foobar", true)
+// Warn logs a warning message
 func Warn(message string, args ...interface{}) {
-	silent := false
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("warn", message, args...)
 	}
-	flogg("info", message, silent, args...)
 }
 
-// Flog: File Logging -> error
-//
-// Prints to file and console by default, override it by passing true to make it silent
-//
-//	log.Error("Foobar", true)
+// Error logs an error message
 func Error(message string, args ...interface{}) {
-	silent := false
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("error", message, args...)
 	}
-	flogg("error", message, silent, args...)
 }
 
-// Flog: File Logging -> debug
-//
-// Only prints to file by default, override it by passing false
-//
-//	log.Debug("Foobar", false)
+// Debug logs a debug message
 func Debug(message string, args ...interface{}) {
-	silent := true //Def true becaue debug
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("debug", message, args...)
 	}
-	flogg("debug", message, silent, args...)
 }
 
-// Flog: File Logging -> aki
-//
-// Only prints to file by default, override it by passing false
-//
-//	log.AKIServerOutput("Foobar", false)
+// AKIServerOutput logs an AKI server output message
 func AKIServerOutput(message string, args ...interface{}) {
-	silent := true //Def true because its server output
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("aki", message, args...)
 	}
-	flogg("aki", message, silent, args...)
 }
 
-// Flog: File Logging -> mtga
-//
-// Only prints to file by default, override it by passing false
-//
-//	log.MTGAServerOutput("Foobar", false)
+// MTGAServerOutput logs an MTGA server output message
 func MTGAServerOutput(message string, args ...interface{}) {
-	silent := true
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("mtga", message, args...)
 	}
-	flogg("mtga", message, silent, args...)
 }
 
-// Flog: File Logging -> online
-//
-// Prints to file and console by default, override it by passing true to make it silent
-//
-//	log.OnlineLog("Foobar", true)
+// OnlineLog logs an online log message
 func OnlineLog(message string, args ...interface{}) {
-	silent := false
-	if len(args) > 0 {
-		if val, ok := args[0].(bool); ok {
-			silent = val
-			args = args[1:]
-		}
+	if logger != nil {
+		logger.log("online", message, args...)
 	}
-	flogg("online", message, silent, args...)
 }
